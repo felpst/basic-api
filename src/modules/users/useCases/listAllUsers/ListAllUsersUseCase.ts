d95f280e7c85807fbc546ca3fef5fd78ac97bc95
Felipe Veloso <felipe.dearaujo@snhu.edu>
2021-06-28 12:59:48 -0300

    Finishing the controllers

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,14 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error(`User with id ${user_id} does not exist!`);
+    }
+    if (!user.admin) {
+      throw new Error(`User with id ${user_id} is not an admin!`);
+    }
+    return this.usersRepository.list();
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileUseCase.ts
@@ -9,7 +9,11 @@ class ShowUserProfileUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error(`User with id ${user_id} does not exist!`);
+    }
+    return user;
   }
 }
 
